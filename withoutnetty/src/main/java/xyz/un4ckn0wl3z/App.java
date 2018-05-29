f1bd0b069d1aaa099b9d10d5983ff889e6029ab1
devinflydrop <psynoxzhio@rmutsvmail.com>
2018-05-29 16:57:18 +0700

    init

---
+++ withoutnetty/src/main/java/xyz/un4ckn0wl3z/App.java
@@ -0,0 +1,13 @@
+package xyz.un4ckn0wl3z;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ withoutnetty/src/main/java/xyz/un4ckn0wl3z/PlainOioServer.java
@@ -0,0 +1,48 @@
+package xyz.un4ckn0wl3z;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+public class PlainOioServer {
+
+    public static void main(String[] args) throws IOException {
+        new PlainOioServer().serv(8000);
+    }
+
+    public void serv(int port) throws IOException {
+        final ServerSocket socket = new ServerSocket(port);
+        try {
+            for (;;){
+                final Socket clientSocket = socket.accept();
+                System.out.println("Accepted connection from " +
+                clientSocket);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        OutputStream out;
+                        try {
+                            out = clientSocket.getOutputStream();
+                            out.write("Hi!\r\n".getBytes(Charset.forName("UTF-8")));
+                            out.flush();
+                            //clientSocket.close();
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }finally {
+//                            try {
+//                                clientSocket.close();
+//                            }catch (IOException ex){
+//
+//                            }
+                        }
+                    }
+                }).start();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
